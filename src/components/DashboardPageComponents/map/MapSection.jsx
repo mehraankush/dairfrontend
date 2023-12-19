bf02ea79cadc42a3f584f9894749d7e4b00f9c17
mehraankush <mehraankush36@gmail.com>
2023-12-20 00:47:24 +0530

    comment map build error

--- src/components/DashboardPageComponents/map/MapSection.jsx
+++ src/components/DashboardPageComponents/map/MapSection.jsx
@@ -1,13 +1,13 @@
 "use client"
-import { VectorMap } from "@react-jvectormap/core";
-import { worldMill } from "@react-jvectormap/world";
+// import { VectorMap } from "@react-jvectormap/core";
+// import { worldMill } from "@react-jvectormap/world";
 import React from "react";
 import { colorScale, countries } from "./Countries";
 
 function WorldMap() {
   return (
     <div style={{ margin: "auto", width: "300px", height: "300px" }} className="mt-5">
-         <VectorMap
+         {/* <VectorMap
         map={worldMill}
         containerStyle={{
           width: "300px",
@@ -23,7 +23,7 @@ function WorldMap() {
             },
           ],
         }}
-        />
+        /> */}
     </div>
   );
 }
